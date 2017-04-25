bffde7fa9d683e6b263fee287ba0410ccf6e3cc1
hgirish <girish@post.com>
2017-04-24 18:50:22 -0700

    outlier with salary more than 1 mil and bonus more than 5 mil

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,7 +10,17 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 removed = data_dict.pop('TOTAL',0)
-print(removed)
+#print(removed)
+
+for k, v in data_dict.iteritems():
+ if type(v) is dict:
+     if float(v['salary']) >= 1000000 and float(v['bonus']) >= 5000000. :
+         print(k,v['salary'], v['bonus'])
+
+  #for t, c in v.iteritems() 
+ #  if t == 'bonus' and float(c) >= 5000000. and c != 'NaN':
+#     print(t, c, k)
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
