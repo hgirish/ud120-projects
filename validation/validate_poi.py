83a30cc98961853d666a35a9f860eb523000a96f
hgirish <girish@post.com>
2017-04-28 23:03:04 -0700

    Quiz 14.18

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -27,13 +27,18 @@ labels, features = targetFeatureSplit(data)
 
 
 
+
+
 ### it's all yours from here forward!  
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
+# Split into a training and testing set
+feature_train, feature_test, label_train, label_test = train_test_split(features, labels, test_size=0.30, random_state=42)
 clf = DecisionTreeClassifier()
-clf.fit(features, labels)
+clf.fit(feature_train, label_train)
 
-pred = clf.predict(features)
-accuracy = accuracy_score(labels, pred)
+pred = clf.predict(feature_test)
+accuracy = accuracy_score(label_test, pred)
 print("The accuracy is: {0}".format(accuracy))
